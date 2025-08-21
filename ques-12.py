176063c95fa7a3aa733e7da8c543571e9306f0a3
Piyush Rathore <piyushrathor152@gmail.com>
2025-08-21 11:09:58 +0530

    Add files via upload

---
+++ ques-12.py
@@ -0,0 +1,4 @@
+a = int(input("enter the first nun:"))
+b = int(input("enter the second num:"))
+power=a**b
+print(power)
---
+++ ques-5.py
@@ -0,0 +1,7 @@
+x=int(input("enter a number: "))
+sum=0
+for i in range (1,x+1):
+  sum+=i
+
+print(sum)
+  
